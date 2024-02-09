bf70e6ae0b275626ca51d983716f773ac944396b
Carlos Sanz <carlos.sanzpenas@gmail.com>
2024-02-09 03:46:47 +0100

    fix: :bug: github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sanzcarlos.github.io/',
-    base: '/public'
+    site: 'https://sanzcarlos.github.io/'
 });
